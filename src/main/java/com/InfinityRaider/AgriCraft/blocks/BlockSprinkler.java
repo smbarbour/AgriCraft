27972fd976893c2d5bf8c5839a232d41cb56476e
Simon Zuend <simon.zuend@gmx.net>
2015-02-02 11:20:42 +0100

    Fixes compilation errors in BlockSrpinkler.

--- src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSprinkler.java
+++ src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSprinkler.java
@@ -3,21 +3,22 @@ package com.InfinityRaider.AgriCraft.blocks;
 import com.InfinityRaider.AgriCraft.init.Items;
 import com.InfinityRaider.AgriCraft.reference.Constants;
 import com.InfinityRaider.AgriCraft.tileentity.TileEntitySprinkler;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class BlockSprinkler extends BlockContainer {
+
     public BlockSprinkler() {
         super(Material.iron);
         this.setHardness(2.0F);
@@ -29,7 +30,6 @@ public class BlockSprinkler extends BlockContainer {
         this.minZ = this.minX;
         this.maxY = Constants.unit*20;
         this.minY = Constants.unit*12;
-
     }
 
     @Override
@@ -38,50 +38,50 @@ public class BlockSprinkler extends BlockContainer {
     }
 
     @Override
-    public boolean isReplaceable(IBlockAccess world, int x, int y, int z) {
+    public boolean isReplaceable(World worldIn, BlockPos pos) {
         return false;
     }
 
     @Override
-    public void harvestBlock(World world, EntityPlayer player, int x, int y, int z, int meta) {
-        if((!world.isRemote) && (!player.isSneaking())) {
-            if(!player.capabilities.isCreativeMode) {       //drop items if the player is not in creative
-                this.dropBlockAsItem(world, x, y, z, meta, 0);
+    public void harvestBlock(World world, EntityPlayer player, BlockPos pos, IBlockState state, TileEntity te) {
+        if (!world.isRemote && !player.isSneaking()) {
+            if (!player.capabilities.isCreativeMode) {       //drop items if the player is not in creative
+                dropBlockAsItem(world, pos, state, 0);
             }
-            world.setBlockToAir(x,y,z);
-            world.removeTileEntity(x,y,z);
+            world.setBlockToAir(pos);
+            world.removeTileEntity(pos);
         }
     }
 
     @Override
-    public void dropBlockAsItemWithChance(World world, int x, int y, int z, int meta, float f, int i) {
-        if(!world.isRemote) {
-            ItemStack drop = new ItemStack(Items.sprinkler, 1);
-            this.dropBlockAsItem(world, x, y, z, drop);
+    public void dropBlockAsItemWithChance(World world, BlockPos pos, IBlockState state, float chance, int fortune) {
+        if (!world.isRemote) {
+            // TODO: override getDrops so that the sprinkler is really dropped
+            // ItemStack drop = new ItemStack(Items.sprinkler, 1);
+            dropBlockAsItem(world, pos, state, 0);
         }
     }
 
     @Override
-    public void onNeighborBlockChange(World world, int x, int y, int z, Block block) {
-        //check if crops can stay
-        if(!this.canBlockStay(world,x,y,z)) {
-            //the crop will be destroyed
-            this.dropBlockAsItem(world, x, y, z, world.getBlockMetadata(x, y, z), 0);
-            world.setBlockToAir(x,y,z);
-            world.removeTileEntity(x,y,z);
+    public void onNeighborBlockChange(World world, BlockPos pos, IBlockState state, Block neighborBlock) {
+        if (!canBlockStay(world, pos)) {
+            dropBlockAsItem(world, pos, state, 0);
+            world.setBlockToAir(pos);
+            world.removeTileEntity(pos);
         }
     }
 
+    // TODO: check what happened to the 'canBlockStay' method
     //see if the block can stay
-    @Override
-    public boolean canBlockStay(World world, int x, int y, int z) {
-        Block channel = world.getBlock(x,y+1,z);
-        return (channel==com.InfinityRaider.AgriCraft.init.Blocks.blockWaterChannel);
+    // @Override
+    public boolean canBlockStay(World world, BlockPos pos) {
+        Block channel = world.getBlockState(pos.up()).getBlock();
+        return channel == com.InfinityRaider.AgriCraft.init.Blocks.blockWaterChannel;
     }
 
     @Override
     @SideOnly(Side.CLIENT)
-    public Item getItem(World world, int x, int y, int z) {
+    public Item getItem(World worldIn, BlockPos pos) {
         return Items.sprinkler;
     }
 
@@ -90,15 +90,19 @@ public class BlockSprinkler extends BlockContainer {
     public int getRenderType() {return -1;}                 //get default render type: net.minecraft.client.renderer
     @Override
     public boolean isOpaqueCube() {return false;}           //tells minecraft that this is not a block (no levers can be placed on it, it's transparent, ...)
-    @Override
-    public boolean renderAsNormalBlock() {return false;}    //tells minecraft that this has custom rendering
+
     @Override
     @SideOnly(Side.CLIENT)
-    public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int i) {return false;}
+    public boolean shouldSideBeRendered(IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+        return false;
+    }
 
+    // TODO: textures in 1.8?
+    /*
     @Override
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int meta) {
         return Blocks.planks.getIcon(0, 0);
     }
+    */
 }
