c611cc2a82d03ffeed9cfbf0ec3f202c02d9deef
Simon Zuend <simon.zuend@gmx.net>
2015-02-02 14:10:50 +0100

    Fixes compilation errors in ItemDebugger.

--- src/main/java/com/InfinityRaider/AgriCraft/items/ItemDebugger.java
+++ src/main/java/com/InfinityRaider/AgriCraft/items/ItemDebugger.java
@@ -1,11 +1,10 @@
 package com.InfinityRaider.AgriCraft.items;
 
 import com.InfinityRaider.AgriCraft.utility.DebugHelper;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
 
 public class ItemDebugger extends ModItem {
@@ -15,14 +14,17 @@ public class ItemDebugger extends ModItem {
     }
 
     @Override
-    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
-        DebugHelper.debug(player, world, x, y ,z);
+    public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
+        DebugHelper.debug(playerIn, worldIn, pos);
         return false;
     }
 
+    // TODO: textures in 1.8?
+    /*
     @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister reg) {
         this.itemIcon = reg.registerIcon(this.getUnlocalizedName().substring(this.getUnlocalizedName().indexOf('.')+1));
     }
+    */
 }
--- src/main/java/com/InfinityRaider/AgriCraft/utility/DebugHelper.java
+++ src/main/java/com/InfinityRaider/AgriCraft/utility/DebugHelper.java
@@ -2,8 +2,10 @@ package com.InfinityRaider.AgriCraft.utility;
 
 import com.InfinityRaider.AgriCraft.utility.interfaces.IDebuggable;
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 
@@ -11,9 +13,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class DebugHelper {
+
+    public static void debug(EntityPlayer player, World world, BlockPos pos) {
+        debug(player, world, pos.getX(), pos.getY(), pos.getZ());
+    }
+
     public static void debug(EntityPlayer player, World world, int x, int y, int z) {
         ArrayList<String> list = new ArrayList<String>();
-        getDebugData(world, x, y,z, list);
+        getDebugData(world, new BlockPos(x, y,z), list);
         for(String data:list) {
             LogHelper.debug(data);
             player.addChatComponentMessage(new ChatComponentText(data));
@@ -22,13 +29,13 @@ public abstract class DebugHelper {
 
     public static void debug(World world, int x, int y, int z) {
         ArrayList<String> list = new ArrayList<String>();
-        getDebugData(world, x, y,z, list);
+        getDebugData(world, new BlockPos(x, y, z), list);
         for(String data:list) {
             LogHelper.debug(data);
         }
     }
 
-    private static void getDebugData(World world, int x, int y, int z, List<String> list) {
+    private static void getDebugData(World world, BlockPos pos, List<String> list) {
         if (!world.isRemote) {
             list.add("Server debug info:");
             list.add("------------------");
@@ -36,13 +43,14 @@ public abstract class DebugHelper {
             list.add("Client debug info:");
             list.add("------------------");
         }
-        TileEntity tile = world.getTileEntity(x, y, z);
+        TileEntity tile = world.getTileEntity(pos);
         if(tile!=null && tile instanceof IDebuggable) {
             ((IDebuggable) tile).addDebugInfo(list);
         }
         else {
-            list.add("Block: "+ Block.blockRegistry.getNameForObject(world.getBlock(x, y, z)));
-            list.add("Meta: "+world.getBlockMetadata(x, y, z));
+            IBlockState state = world.getBlockState(pos);
+            list.add("Block: "+ Block.blockRegistry.getNameForObject(state.getBlock()));
+            list.add("Meta: "+ state.getBlock().getMetaFromState(state));
         }
     }
 }
