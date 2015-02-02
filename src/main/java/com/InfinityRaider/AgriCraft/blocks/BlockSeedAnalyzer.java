f68283dc605a8c1e6a5d74b03649933dab2246de
Simon Zuend <simon.zuend@gmx.net>
2015-02-02 11:03:45 +0100

    Fixes compilation errors in Seed Analyzer.

--- src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSeedAnalyzer.java
+++ src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSeedAnalyzer.java
@@ -7,26 +7,23 @@ import com.InfinityRaider.AgriCraft.handler.GuiHandler;
 import com.InfinityRaider.AgriCraft.init.Blocks;
 import com.InfinityRaider.AgriCraft.reference.Constants;
 import com.InfinityRaider.AgriCraft.tileentity.TileEntitySeedAnalyzer;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.particle.EffectRenderer;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.IIcon;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MathHelper;
-import net.minecraft.util.MovingObjectPosition;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BlockSeedAnalyzer extends Block implements ITileEntityProvider {
     public BlockSeedAnalyzer() {
@@ -54,52 +51,51 @@ public class BlockSeedAnalyzer extends Block implements ITileEntityProvider {
 
     //this sets the block's orientation based upon the direction the player is looking when the block is placed
     @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
-        if(world.getTileEntity(x, y, z) instanceof TileEntitySeedAnalyzer) {
-            TileEntitySeedAnalyzer analyzer = (TileEntitySeedAnalyzer) world.getTileEntity(x, y, z);
-            int direction = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        if(world.getTileEntity(pos) instanceof TileEntitySeedAnalyzer) {
+            TileEntitySeedAnalyzer analyzer = (TileEntitySeedAnalyzer) world.getTileEntity(pos);
+            int direction = MathHelper.floor_double(placer.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
             switch(direction) {
-                case 0: analyzer.setDirection(ForgeDirection.NORTH.ordinal()); break;
-                case 1: analyzer.setDirection(ForgeDirection.EAST.ordinal()); break;
-                case 2: analyzer.setDirection(ForgeDirection.SOUTH.ordinal()); break;
-                case 3: analyzer.setDirection(ForgeDirection.WEST.ordinal()); break;
+                case 0: analyzer.setDirection(EnumFacing.NORTH.ordinal()); break;
+                case 1: analyzer.setDirection(EnumFacing.EAST.ordinal()); break;
+                case 2: analyzer.setDirection(EnumFacing.SOUTH.ordinal()); break;
+                case 3: analyzer.setDirection(EnumFacing.WEST.ordinal()); break;
             }
         }
     }
 
-    //called when the block is broken
-     @Override
-    public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
-         if(!world.isRemote) {
-             world.removeTileEntity(x, y, z);
-             world.setBlockToAir(x, y, z);
-         }
+    @Override
+    public void breakBlock(World world, BlockPos pos, IBlockState state) {
+        if (!world.isRemote) {
+            world.removeTileEntity(pos);
+            world.setBlockToAir(pos);
+        }
     }
 
     //override this to delay the removal of the tile entity until after harvestBlock() has been called
     @Override
-    public boolean removedByPlayer(World world, EntityPlayer player, int x, int y, int z, boolean willHarvest) {
-        return !player.capabilities.isCreativeMode || super.removedByPlayer(world, player, x, y, z, willHarvest);
+    public boolean removedByPlayer(World world, BlockPos pos, EntityPlayer player, boolean willHarvest) {
+        return !player.capabilities.isCreativeMode || super.removedByPlayer(world, pos, player, willHarvest);
     }
 
     //this gets called when the block is mined
     @Override
-    public void harvestBlock(World world, EntityPlayer player, int x, int y, int z, int meta) {
-        if(!world.isRemote) {
-            if(!player.capabilities.isCreativeMode) {
-                this.dropBlockAsItem(world, x, y, z, meta, 0);
+    public void harvestBlock(World world, EntityPlayer player, BlockPos pos, IBlockState state, TileEntity te) {
+        if (!world.isRemote) {
+            if (!player.capabilities.isCreativeMode) {
+                dropBlockAsItem(world, pos, state, 0);
             }
-            this.breakBlock(world, x, y, z, world.getBlock(x, y, z), meta);
+            breakBlock(world, pos, state);
         }
     }
 
     //get a list with items dropped by the the crop
     @Override
-    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) {
-        ArrayList<ItemStack> items = new ArrayList<ItemStack>();
+    public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+        List<ItemStack> items = new ArrayList<ItemStack>();
         items.add(new ItemStack(Item.getItemFromBlock(Blocks.seedAnalyzer), 1, 0));
-        if (world.getTileEntity(x, y, z) != null && world.getTileEntity(x, y, z) instanceof TileEntitySeedAnalyzer) {
-            TileEntitySeedAnalyzer analyzer = (TileEntitySeedAnalyzer) world.getTileEntity(x, y, z);
+        if (world.getTileEntity(pos) != null && world.getTileEntity(pos) instanceof TileEntitySeedAnalyzer) {
+            TileEntitySeedAnalyzer analyzer = (TileEntitySeedAnalyzer) world.getTileEntity(pos);
             if(analyzer.getStackInSlot(ContainerSeedAnalyzer.seedSlotId)!=null) {
                 items.add(analyzer.getStackInSlot(ContainerSeedAnalyzer.seedSlotId));
             }
@@ -112,12 +108,12 @@ public class BlockSeedAnalyzer extends Block implements ITileEntityProvider {
 
     //open the gui when the block is activated
     @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float fX, float fY, float fZ) {
-        if(player.isSneaking()) {
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (player.isSneaking()) {
             return false;
         }
-        if(!world.isRemote) {
-            player.openGui(AgriCraft.instance, GuiHandler.seedAnalyzerID, world, x, y, z);
+        if (!world.isRemote) {
+            player.openGui(AgriCraft.instance, GuiHandler.seedAnalyzerID, world, pos.getX(), pos.getY(), pos.getZ());
         }
         return true;
     }
@@ -127,16 +123,14 @@ public class BlockSeedAnalyzer extends Block implements ITileEntityProvider {
     public int getRenderType() {return -1;}                 //get default render type: net.minecraft.client.renderer
     @Override
     public boolean isOpaqueCube() {return false;}           //tells minecraft that this is not a block (no levers can be placed on it, it's transparent, ...)
+
     @Override
-    public boolean renderAsNormalBlock() {return false;}    //tells minecraft that this has custom rendering
-    @Override
-    public boolean shouldSideBeRendered(IBlockAccess world, int x, int y, int z, int i) {return false;}
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean addHitEffects(World worldObj, MovingObjectPosition target, EffectRenderer effectRenderer) {return false;}        //no particles when this block gets hit
-    @Override
-    @SideOnly(Side.CLIENT)
-    public boolean addDestroyEffects(World world, int x, int y, int z, int meta, EffectRenderer effectRenderer) {return false;}     //no particles when destroyed
+    public boolean shouldSideBeRendered(IBlockAccess worldIn, BlockPos pos, EnumFacing side) {
+        return false;
+    }
+
+    // TODO: textures in 1.8?
+    /*
     @Override
     @SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister reg) {
@@ -147,11 +141,12 @@ public class BlockSeedAnalyzer extends Block implements ITileEntityProvider {
     public IIcon getIcon(int side, int meta) {
         return net.minecraft.init.Blocks.planks.getIcon(0, 0);
     }
+    */
 
     @Override
-    public boolean onBlockEventReceived(World world, int x, int y, int z, int id, int data) {
-        super.onBlockEventReceived(world, x, y, z, id, data);
-        TileEntity tileEntity = world.getTileEntity(x,y,z);
-        return (tileEntity!=null)&&(tileEntity.receiveClientEvent(id,data));
+    public boolean onBlockEventReceived(World world, BlockPos pos, IBlockState state, int eventID, int eventParam) {
+        super.onBlockEventReceived(world, pos, state, eventID, eventParam);
+        TileEntity tileEntity = world.getTileEntity(pos);
+        return tileEntity != null && tileEntity.receiveClientEvent(eventID, eventParam);
     }
 }
