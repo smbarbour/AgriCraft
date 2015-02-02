202e6e8556ef65aee2ba2bd6d0022090aca3fc78
Simon Zuend <simon.zuend@gmx.net>
2015-02-02 11:12:08 +0100

    Fixes compilation errors in BlockSeedStorage.

--- src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSeedStorage.java
+++ src/main/java/com/InfinityRaider/AgriCraft/blocks/BlockSeedStorage.java
@@ -3,13 +3,15 @@ package com.InfinityRaider.AgriCraft.blocks;
 import com.InfinityRaider.AgriCraft.AgriCraft;
 import com.InfinityRaider.AgriCraft.handler.GuiHandler;
 import com.InfinityRaider.AgriCraft.tileentity.TileEntitySeedStorage;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
-import net.minecraftforge.common.util.ForgeDirection;
 
 public class BlockSeedStorage extends BlockCustomWood {
     public BlockSeedStorage() {
@@ -23,34 +25,34 @@ public class BlockSeedStorage extends BlockCustomWood {
 
     //this sets the block's orientation based upon the direction the player is looking when the block is placed
     @Override
-    public void onBlockPlacedBy(World world, int x, int y, int z, EntityLivingBase entity, ItemStack stack) {
-        if(world.getTileEntity(x, y, z) instanceof TileEntitySeedStorage) {
-            TileEntitySeedStorage te = (TileEntitySeedStorage) world.getTileEntity(x, y, z);
-            int direction = MathHelper.floor_double(entity.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
+    public void onBlockPlacedBy(World world, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack) {
+        if(world.getTileEntity(pos) instanceof TileEntitySeedStorage) {
+            TileEntitySeedStorage te = (TileEntitySeedStorage) world.getTileEntity(pos);
+            int direction = MathHelper.floor_double(placer.rotationYaw * 4.0F / 360.0F + 0.5D) & 3;
             switch(direction) {
-                case 0: te.setDirection(ForgeDirection.NORTH.ordinal()); break;
-                case 1: te.setDirection(ForgeDirection.EAST.ordinal()); break;
-                case 2: te.setDirection(ForgeDirection.SOUTH.ordinal()); break;
-                case 3: te.setDirection(ForgeDirection.WEST.ordinal()); break;
+                case 0: te.setDirection(EnumFacing.NORTH.ordinal()); break;
+                case 1: te.setDirection(EnumFacing.EAST.ordinal()); break;
+                case 2: te.setDirection(EnumFacing.SOUTH.ordinal()); break;
+                case 3: te.setDirection(EnumFacing.WEST.ordinal()); break;
             }
         }
     }
 
     @Override
-    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float fX, float fY, float fZ) {
-        if(player.isSneaking()) {
+    public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer player, EnumFacing side, float hitX, float hitY, float hitZ) {
+        if (player.isSneaking()) {
             return false;
         }
-        if(!world.isRemote) {
-            player.openGui(AgriCraft.instance, GuiHandler.seedStorageID, world, x, y, z);
+        if (!world.isRemote) {
+            player.openGui(AgriCraft.instance, GuiHandler.seedStorageID, world, pos.getX(), pos.getY(), pos.getZ());
         }
         return true;
     }
 
     @Override
-    public boolean onBlockEventReceived(World world, int x, int y, int z, int id, int data) {
-        super.onBlockEventReceived(world, x, y, z, id, data);
-        TileEntity tileentity = world.getTileEntity(x, y, z);
-        return tileentity != null && tileentity.receiveClientEvent(id, data);
+    public boolean onBlockEventReceived(World world, BlockPos pos, IBlockState state, int eventID, int eventParam) {
+        super.onBlockEventReceived(world, pos, state, eventID, eventParam);
+        TileEntity tileentity = world.getTileEntity(pos);
+        return tileentity != null && tileentity.receiveClientEvent(eventID, eventParam);
     }
 }
